9679452b652c9b48f7d7d5e3f90ade4f5e194bfd
Rishav Mukherjee <rishavmukherjee11@gmail.com>
2024-01-28 17:02:39 +0530

    ok

--- src/prisma/seed.ts
+++ src/prisma/seed.ts
@@ -1,7 +1,2 @@
 import { PrismaClient,Prisma } from "@prisma/client";
 const prisma = new PrismaClient()
-async function main() {
-  const user = await prisma.user.create({
-    data: {
-      name: "Alice",
-      email: ""
