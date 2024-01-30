1930c66ed1294c715d9b97ce2e479a5669329ed0
Rishav Mukherjee <rishavmukherjee11@gmail.com>
2024-01-30 21:22:08 +0530

    kk

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -33,7 +33,7 @@ const App = () => {
     if (token) {
       router.push('/home/dashboard');
     }
-  }, []);
+  }, );
   return (
     <div className="min-h-screen bg-gray-100 py-6 flex flex-col justify-center sm:py-12"
      style={{backgroundImage: "url('/protrudingsquares.svg')",   }}>
